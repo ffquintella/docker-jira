76c967dd5761a13fb68553391407b974880e89c3
Felipe F Quintella <felipe.quintella@fgv.br>
2016-10-07 16:19:05 -0300

    FIX

--- start-service.sh
+++ start-service.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-/opt/puppetlabs/puppet/bin/puppet apply -l /var/log/puppet.log --modulepath=/etc/puppet/modules /etc/puppet/manifests/start.pp &
+/opt/puppetlabs/puppet/bin/puppet apply -l /var/log/puppet.log --modulepath=/etc/puppet/modules /etc/puppet/manifests/start.pp
 
 while [ ! -f ${FACTER_JIRA_HOME}/log/atlassian-jira.log ]
 do
